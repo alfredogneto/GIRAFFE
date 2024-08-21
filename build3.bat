f92c076e2bf69ced127507bc7153a65b00ae75f1
Alfredo Gay Neto <67784667+alfredogneto@users.noreply.github.com>
2024-08-21 18:48:10 -0300

    Update build3.bat

--- build3.bat
+++ build3.bat
@@ -5,16 +5,7 @@ cmake ..
 :: Build Giraffe (Debug)
 cmake --build . --config Debug
 
-
-
-::Include in the system PATH:
-::..\dependencies\vcpkg\packages\arpack-ng_x64-windows\bin\
-::..\dependencies\vcpkg\packages\lapack-reference_x64-windows\bin\
-::..\dependencies\vcpkg\packages\openblas_x64-windows\bin\
-::..\dependencies\vcpkg\packages\vcpkg-gfortran_x64-windows\bin\
-::C:\Program Files (x86)\Intel\oneAPI\mkl\2024.2\bin
-
-::useful references:
+::Useful references:
 ::https://github.com/opencollab/arpack-ng#windows-support
 ::https://www.dealii.org/developer/external-libs/arpack.html
 ::https://github.com/opencollab/arpack-ng.git
