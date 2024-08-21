85c2f4466ae5492954d8a5a35f2e299a4d54c342
Alfredo Gay Neto <67784667+alfredogneto@users.noreply.github.com>
2024-08-21 18:46:17 -0300

    Update build4.bat

--- build4.bat
+++ build4.bat
@@ -1,6 +1,7 @@
 setx GIRAFFE_INSTALL "%cd%\dependencies\vcpkg\packages"
 setx GIRAFFE_PATH %%GIRAFFE_INSTALL%%\arpack-ng_x64-windows\bin\;%%GIRAFFE_INSTALL%%\lapack-reference_x64-windows\bin\;%%GIRAFFE_INSTALL%%\openblas_x64-windows\bin\;%%GIRAFFE_INSTALL%%\vcpkg-gfortran_x64-windows\bin;
-setx MKL_PATH "C:\Program Files (x86)\Intel\oneAPI\mkl\2024.2\bin"
+setx MKL_PATH "C:\Program Files (x86)\Intel\oneAPI\mkl\latest\bin;C:\Program Files (x86)\Intel\oneAPI\compiler\latest\bin"
+setx ADD_PATH %%GIRAFFE_PATH%%;%%MKL_PATH%%;
 
 ::Include in the system PATH:
 ::..\dependencies\vcpkg\packages\arpack-ng_x64-windows\bin\
