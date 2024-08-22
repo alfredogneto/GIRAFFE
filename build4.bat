15f861036d781762e307c4cfcc12ee67f084be46
Alfredo Gay Neto <67784667+alfredogneto@users.noreply.github.com>
2024-08-22 15:19:59 -0300

    Update build4.bat

--- build4.bat
+++ build4.bat
@@ -1,7 +1,6 @@
 setx GIRAFFE_INSTALL "%cd%\dependencies\vcpkg\packages"
-setx GIRAFFE_PATH %%GIRAFFE_INSTALL%%\arpack-ng_x64-windows\bin\;%%GIRAFFE_INSTALL%%\lapack-reference_x64-windows\bin\;%%GIRAFFE_INSTALL%%\openblas_x64-windows\bin\;%%GIRAFFE_INSTALL%%\vcpkg-gfortran_x64-windows\bin;
-setx MKL_PATH "C:\Program Files (x86)\Intel\oneAPI\mkl\latest\bin;C:\Program Files (x86)\Intel\oneAPI\compiler\latest\bin"
-setx ADD_PATH %%GIRAFFE_PATH%%;%%MKL_PATH%%;
+setx GIRAFFE_PATH %%GIRAFFE_INSTALL%%\arpack-ng_x64-windows\bin\;%%GIRAFFE_INSTALL%%\lapack-reference_x64-windows\bin\;%%GIRAFFE_INSTALL%%\openblas_x64-windows\bin\;%%GIRAFFE_INSTALL%%\vcpkg-gfortran_x64-windows\bin\;
+setx MKL_PATH "C:\Program Files (x86)\Intel\oneAPI\mkl\latest\bin\;C:\Program Files (x86)\Intel\oneAPI\compiler\latest\bin\;"
 
-::The last step is to include in the system PATH the environment variable %ADD_PATH%.
+::The last step is to include in the system PATH the environment variables: %GIRAFFE_PATH% and %MKL_PATH%
 ::This is not done automatically by this script routine.
